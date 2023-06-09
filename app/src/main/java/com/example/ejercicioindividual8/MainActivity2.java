a26436a5f6c9c8324c66229926bde305d9aac162
ivette2021 <iconstanzav1@gmail.com>
2023-06-09 15:47:51 -0400

    ejercicioIndividual8

---
+++ app/src/main/java/com/example/ejercicioindividual8/MainActivity2.java
@@ -0,0 +1,65 @@
+package com.example.ejercicioindividual8;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+    private String TAG = MainActivity2.class.getSimpleName();
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button= findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.youtube.com";
+                Intent mIntent = new Intent(Intent.ACTION_VIEW);
+                mIntent.setData(Uri.parse(url));
+                startActivity(mIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+}
